d1e628b303c5ab30d355cfc85686cdadf67b833d
jvsoarez <joaovictorcpsa@gmail.com>
2023-02-17 00:57:51 -0300

    feat: component App
    renders the other components in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import './App.css'
+import './App.css';
+import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 
 function App() {
-  return (
-    <div className="App">
-      OLÁ, MUNDO!
-    </div>
-  )
+	return (
+		<div className="App">
+			<Header />
+      <SolarSystem />
+		</div>
+	);
 }
 
-export default App
+export default App;
